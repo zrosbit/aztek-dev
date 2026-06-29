fddef603483341c312171db04d092e1641aa1cef
zrosbit <zrosbit@gmail.com>
2026-06-29 09:03:47 +0530

    Dashboard-Page-update

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation"
+// import { redirect } from "next/navigation"
 
-export default function DashboardRedirect() {
-  redirect("/overview")
+// export default function DashboardRedirect() {
+//   redirect("/overview")
+// }
+
+
+import { redirect } from "next/navigation";
+
+export default function DashboardPage() {
+  redirect("/overview");
+
+  return null;
 }

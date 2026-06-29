25439436e9e00282fd05ac3a619557491b2107fe
zrosbit <zrosbit@gmail.com>
2026-06-29 09:25:42 +0530

    DashboardPageRemove

--- src/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-// import { redirect } from "next/navigation"
-
-// export default function DashboardRedirect() {
-//   redirect("/overview")
-// }
-
-export default function DashboardPage() {
-  return <div>Dashboard Home</div>;
-}
